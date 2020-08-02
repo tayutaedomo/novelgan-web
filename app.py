e7ac137cec912c2391c7b27347461c3205c34223
tayutaedomo <tayutaedomo@gmail.com>
2020-08-03 02:26:45 +0900

    Add predict logic

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
 from flask import Flask, render_template, request, redirect, url_for
 from flask_httpauth import HTTPBasicAuth
 
@@ -8,6 +11,12 @@ APP_SETTINGS = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
 app.config.from_object(APP_SETTINGS)
 
 auth = HTTPBasicAuth()
+
+from lib.cyclegan import CycleGAN, IMG_COLS, IMG_ROWS
+
+gan = CycleGAN()
+gan.init()
+gan.load_models()
 
 
 @auth.verify_password
@@ -26,7 +35,9 @@ def index():
 
 @app.route('/cyclegan', methods=['GET', 'POST'])
 def cyclegan():
-    local = {}
+    local = {
+        'predicted': False
+    }
 
     if request.files.get('image'):
         file = request.files['image']
@@ -34,6 +45,23 @@ def cyclegan():
 
         content = file.read()
         local['file_base64'] = str(base64.b64encode(content), 'utf-8')
+
+        img = Image.open(BytesIO(content))
+        img_resize = img.resize((IMG_COLS, IMG_ROWS))
+        img_np = np.array(img_resize) / 127.5 - 1.
+        img_reshape = img_np.reshape(1, IMG_ROWS, IMG_COLS, 3)
+
+        x = img_reshape
+        y = gan.generate_image(x)
+        y = (0.5 * y + 0.5) * 255
+        y_img = Image.fromarray(np.uint8(y[0]))
+
+        buffered = BytesIO()
+        y_img.save(buffered, format='JPEG')
+        y_img_str = str(base64.b64encode(buffered.getvalue()), 'utf-8')
+
+        local['predicted'] = True
+        local['y_proba'] = y_img_str
 
     return render_template('cyclegan.html', local=local)
 
